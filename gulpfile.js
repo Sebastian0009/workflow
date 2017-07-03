9d9109aab14dc646a12d71643b8997188b3939cb
Sebastian0009 <stoichitasebastian@gmail.com>
2017-07-03 14:46:51 +0300

    gulp sass and gulp added

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Sebi on 7/3/2017.
+ */
+
+var gulp = require('gulp');
+var sass = require ('gulp-sass');
+
+
+gulp.task('sass',function(){
+   return gulp.src('src/scss/app.scss')
+       .pipe(sass({outputStyle:'expanded'}).on('error',sass.logError))
+       .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('default', ['sass']);
